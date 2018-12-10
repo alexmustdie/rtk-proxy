1934c981756e13d6ce54f2a7ad292fd1ee250d8a
Aleksei Kovalenko <a.kovalenko@geoscan.aero>
2018-12-10 20:20:09 +0300

    delete AlertBox.py

--- UI/AlertBox.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QMessageBox
-
-class AlertBox(QMessageBox):
-
-  def __init__(self, message):
-    super().__init__()
-    self.setIcon(QMessageBox.Critical)
-    self.setWindowTitle('Ошибка')
-    self.setText(message)
